15c01a90c362c7dd07384e7ab059e13f7b274c80
Nostalgintosh Classic <matthewcoto@proton.me>
2026-07-21 23:49:54 -0400

    Fix typo in COBOL program message

--- why.cbl
+++ why.cbl
@@ -9,6 +9,6 @@
             PROGRAM-ID. MAIN.
 
             PROCEDURE DIVISION.
-               DISPLAY "Hello, Hell! I do not recommend COLBOL"
+               DISPLAY "Hello, Hell! I do not recommend COBOL"
                DISPLAY "That one COBOL programmer in is mid 80s asking WHY?"
                STOP RUN.
