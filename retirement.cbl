180ce0df06c62a90aa57e53d1a43774778582bd8
Nostalgintosh Classic <matthewcoto@proton.me>
2026-07-21 23:56:09 -0400

    Add retirement message in COBOL program

---
+++ retirement.cbl
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+            IDENTIFICATION DIVISION.
+            PROGRAM-ID. MAIN.
+
+            PROCEDURE DIVISION.
+               DISPLAY "If you can read and write COBOL I have one thing to tell you"
+               DISPLAY "FINALLY, I can retire! I am 89 year old and finally done."
+               STOP RUN.
